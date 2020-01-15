329beea80890468148fbb1415974c3de5010c0c0
Mitchell T Alton <mitchell.alton@live.com>
2020-01-15 00:31:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,7 +44,7 @@ def total_even_pairs(src)
   i = 0 
   while i < src.count do 
     if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
-      new_array << src[i]
+      new_array << src[i][0] + src[i][1]
     end
     i += 1 
   end
